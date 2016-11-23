2cf656829db4bfee5ccb4005abf06b1072175f05
baekcedar <baekcedar@gmail.com>
2016-11-23 17:43:54 +0900

    first

---
+++ app/src/main/java/com/hm/project_glue/LogoActivity.java
@@ -0,0 +1,32 @@
+package com.hm.project_glue;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class LogoActivity extends AppCompatActivity {
+    Handler mHandler;
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        context = this;
+        mHandler = new Handler();
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, "Activity Thread.sleep Test", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LogoActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000); // 1000ms
+
+    }
+
+}
