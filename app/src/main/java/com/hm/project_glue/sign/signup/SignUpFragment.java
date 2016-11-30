c2b177113d7ff281e3cb4c1f5aaa7082edb81ab8
손종국 <conquerex@gmail.com>
2016-11-30 19:43:14 +0900

    SignUp 복구

--- app/src/main/java/com/hm/project_glue/sign/signup/SignUpFragment.java
+++ app/src/main/java/com/hm/project_glue/sign/signup/SignUpFragment.java
@@ -6,15 +6,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-
 import com.hm.project_glue.R;
 
-public class SignUpFragment extends Fragment implements SignUpPresenter {
 
-    Button btnSuSignUp;
-    EditText etSuId, etSuPw, etSuPwRe, etSuEmail, etSuPhone, etSuName;
+
+public class SignUpFragment extends Fragment {
 
     public SignUpFragment() {
 
@@ -22,6 +18,8 @@ public class SignUpFragment extends Fragment implements SignUpPresenter {
 
     public static SignUpFragment newInstance() {
         SignUpFragment fragment = new SignUpFragment();
+
+
         return fragment;
     }
 
@@ -34,28 +32,11 @@ public class SignUpFragment extends Fragment implements SignUpPresenter {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
-        etSuId = (EditText)view.findViewById(R.id.etId);
-        etSuPw = (EditText)view.findViewById(R.id.etPassword);
-        etSuPwRe = (EditText)view.findViewById(R.id.etRePassword);
-        etSuName = (EditText)view.findViewById(R.id.etName);
-        etSuPhone = (EditText)view.findViewById(R.id.etPhone);
-        etSuEmail = (EditText)view.findViewById(R.id.etEmail);
-
-        // 회원가입 버튼 클릭
-        btnSuSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
         Log.i("test","fragment_sign_up-onCreateView");
-        return view;
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
 
-    @Override
-    public void signUp() {
 
-    }
+
 }
