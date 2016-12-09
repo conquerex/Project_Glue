9eabe66cb191b98f62dbd024ecae0f3e131c3ff3
손종국 <conquerex@gmail.com>
2016-12-09 17:00:02 +0900

    Mid commit - test2

--- app/src/main/java/com/hm/project_glue/main/home/HomePresenterImpl.java
+++ app/src/main/java/com/hm/project_glue/main/home/HomePresenterImpl.java
@@ -64,6 +64,7 @@ public class HomePresenterImpl implements HomePresenter {
 
                         작성 예정
                         테스트
+                        test2
                         
                          */
                     }
